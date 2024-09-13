f9f17d158a935f7f0d374316203623d2169a0bec
Vinicius Faria <vinicius.f4ria@gmail.com>
2024-09-13 14:18:30 -0300

    add functions calculator

--- my_calculator/bin/my_calculator.dart
+++ my_calculator/bin/my_calculator.dart
@@ -14,26 +14,26 @@ void main(List<String> arguments) {
   if(line != null){
     var values = line.split(' ');
     if(values.length >= 2){
-      var num1 = int.parse(values[0]);
-      var num2 = int.parse(values[1]);
+      var num1 = double.parse(values[0]);
+      var num2 = double.parse(values[1]);
 
 
       switch (op) {
         case "+":
-          print("O resultado eh: ${num1+num2}"); 
+          print("O resultado eh: ${soma(num1,num2)}"); 
           break;
         case "-":
-          print("O resultado eh: ${num1-num2}"); 
+          print("O resultado eh: ${sub(num1,num2)}"); 
           break;
         case "*":
-          print("O resultado eh: ${num1*num2}"); 
+          print("O resultado eh: ${multi(num1,num2)}"); 
           break;
-        case "-":
+        case "/":
           if(num2 <= 0){
             print("Não é possivel dividir por 0");
             break;
           } else {
-            print("O resultado eh: ${num1/num2}"); 
+            print("O resultado eh: ${div(num1,num2)}"); 
             break;
           }
         default:
@@ -41,3 +41,22 @@ void main(List<String> arguments) {
     }
   }
 }
+
+
+double soma( double numero1, double numero2){
+  return numero1 + numero2;
+}
+
+double sub( double numero1, double numero2){
+  return numero1 - numero2;
+}
+
+double multi( double numero1, double numero2){
+  return numero1 * numero2;
+}
+
+double div( double numero1, double numero2){
+  return numero1/numero2;
+}
+
+
